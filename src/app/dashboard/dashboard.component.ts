bfb9c28e71c9960e1b64c180e74b56d30fe32c32
kiranbollepalli9 <kiranbollepalli@gmail.com>
2020-06-16 19:36:11 +0530

    Added pagination and sort

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { RestService } from '../rest.service';
 import { CountryReport } from 'src/CountryReport';
 import { MatTableDataSource } from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
 
 
 @Component({
@@ -13,11 +16,17 @@ export class DashboardComponent implements OnInit {
   displayedColumns: string[] = ['country', 'cases', 'todayCases', 'deaths', 'todayDeaths', 'recovered', 'todayRecovered'];
   countrywiseReports: CountryReport[];
   dataSource = new MatTableDataSource<CountryReport>(this.countrywiseReports);
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
 
 
   constructor(private service: RestService) { }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+
     this.getCountrywiseReports();
   }
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
-
-
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 const MaterialComponents = [
   MatButtonModule,
   MatToolbarModule,
-  MatTableModule
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
 ];
 
 @NgModule({
